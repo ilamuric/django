ba79d9a0a7d763517875e03acc14771f24194b95
ilamuric <81032987+ilamuric@users.noreply.github.com>
2023-07-25 17:12:46 +0300

    Updated ALLOWED_HOSTS in settings.py

--- first/settings.py
+++ first/settings.py
@@ -139,5 +139,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')  # Добавьте эту строку
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
